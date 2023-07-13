9803a1ef728180a60ddb26961add730e112923de
smart94423 <smart94423@gmail.com>
2023-07-13 12:06:23 +0200

    fix: improve err message

--- src/page.ts
+++ src/page.ts
@@ -1,13 +1,15 @@
 export { page }
 
-import { assert } from './utils'
 import { getCurrentTest } from './getCurrentTest'
 import type { Page } from 'playwright-chromium'
 
 const page = new Proxy({} as Page, {
   get(_, prop) {
     const { page } = getCurrentTest()
-    assert(page)
+    if (!page)
+      throw new Error(
+        "`page` isn't available. Make sure to access `page` only inside test() calls. In particular, if test() is async, then make sure to await all asynchronous code executed by your async test()."
+      )
     return page[prop as keyof Page]
   },
 })
