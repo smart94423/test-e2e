3bafb1c67b0813fe4a6dd8a140ac557cc4da7e7e
smart94423 <smart94423@gmail.com>
2023-08-22 13:15:03 +0200

    minor refactor

--- src/utils/cliConfig.ts
+++ src/utils/cliConfig.ts
@@ -1,6 +1,37 @@
-const cliArgs = process.argv.slice(2)
-export const cliConfig = {
-  debugEsbuild: cliArgs.includes('--debug-esbuild'),
+export { cliConfig }
+
+const options = process.argv.filter((w) => w.startsWith('--'))
+
+const INSPECT = '--inspect'
+let inspect = false
+if (includes(options, INSPECT)) {
+  inspect = true
+}
+
+const DEBUG_ESBUILD = '--debug-esbuild'
+let debugEsbuild = false
+if (includes(options, DEBUG_ESBUILD)) {
+  debugEsbuild = true
+}
+
+if (options.length > 0) {
+  throw new Error(
+    [
+      // prettier-ignore
+      `unknown options: ${options.join(' ')}`,
+      `known options: ${[INSPECT, DEBUG_ESBUILD].join(' ')}`,
+    ].join('\n')
+  )
+}
+
+const cliConfig = {
   // To inspect a specific test: `$ touch examples/some-example/INSPECT`
-  inspect: cliArgs.includes('--inspect') || !!process.env.TEST_INSPECT,
+  inspect: inspect || !!process.env.TEST_INSPECT,
+  debugEsbuild,
+}
+
+function includes(list: string[], str: string) {
+  const val = list.includes(str)
+  if (val) list = list.filter((s) => s !== str)
+  return val
 }
